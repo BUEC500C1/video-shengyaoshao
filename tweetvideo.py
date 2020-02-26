04c4e95f8f3f90f0346bed90915bc7357bdeab7e
shengyaoshao <55214118+shengyaoshao@users.noreply.github.com>
2020-02-26 01:20:03 -0500

    Add files via upload

--- tweetvideo.py
+++ tweetvideo.py
@@ -3,11 +3,13 @@ import os
 import io
 import urllib.request
 import subprocess
-#import glob
-#import stat
-import tweet_keys
+import glob
+import stat
 from google.cloud import vision
 from google.cloud.vision import types
+import tweet_keys
+from PIL import Image, ImageDraw, ImageFont
+import PIL
 
 consumer_key = tweet_keys.consumer_key
 consumer_secret = tweet_keys.consumer_secret
@@ -57,8 +59,8 @@ def get_all_tweets(account1,number1,filepath):
                 else:
                         
                         outtweets.append([tweet.id_str, tweet.created_at, tweet.text.encode("utf-8"), tweet.entities['media'][0]['media_url']])
-        imagenum = fileorder - 1
-        print(str(imagenum)+'images detected')
+        #imagenum = fileorder - 1
+        #print(str(imagenum)+'images detected')
         return fileorder
 def googlevision(account1,number1,filepath1, filepath2):
         filenumber = int(get_all_tweets(account1,number1,filepath1))
@@ -69,56 +71,52 @@ def googlevision(account1,number1,filepath1, filepath2):
         if(filenumber == 1):
                 print("No image posted by this account in the recent tweets")
         while(file_name < filenumber):
+                addtxt = Image.open(os.path.join(path, str(file_name) + '.jpg'))
+                draw = ImageDraw.Draw(addtxt)
                 with io.open(os.path.join(path, str(file_name) + '.jpg'),'rb') as image_file:
                         content = image_file.read()
                 image = vision.types.Image(content = content)
-                response = client.text_detection(image = image)
-                texts = response.text_annotations
-                print('\nTexts of Image' + str(file_name) + ':')
-                z = 0
-                for text in texts:
-                        z = 1
-                        print(text.description)
-                if (z == 0):
-                        print('No text detected')
-                if response.error.message:
-                        raise Exception(
-                                '{}\nFor more info on error messages, check: '
-                                'https://cloud.google.com/apis/design/errors'.format(
-                                response.error.message))
-
                 response = client.face_detection(image=image)
                 faces = response.face_annotations
                 # Names of likelihood from google.cloud.vision.enums
                 likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
                        'LIKELY', 'VERY_LIKELY')
-                print('\nFaces of Image' + str(file_name) + ':')
                 person = 1
-                x = 0
+                flag = 0
+                (x, y) = (50,200)
+                font = ImageFont.truetype('Roboto-Bold.ttf', size=20)
+                color = 'rgb(213,53,123)'
                 for face in faces:
-                        x = 1
+                        flag = 1
                         if (likelihood_name[face.anger_likelihood] == 'VERY_LIKELY'):
-                                print('Person' + str(person) + ' is very likely to be angery')
+                                message = ('Person' + str(person) + ' is very likely to be angery')
                         elif (likelihood_name[face.anger_likelihood] == 'LIKELY'):
-                                print('Person' + str(person) + ' is likely to be angery')
+                                message = ('Person' + str(person) + ' is likely to be angery')
                         elif (likelihood_name[face.joy_likelihood] == 'VERY_LIKELY'):
-                                print('Person' + str(person) + ' is very likely to be happy')
+                                message = ('Person' + str(person) + ' is very likely to be happy')
                         elif (likelihood_name[face.joy_likelihood] == 'LIKELY'):
-                                print('Person' + str(person) + ' is likely to be happy')
+                                message = ('Person' + str(person) + ' is likely to be happy')
                         elif (likelihood_name[face.surprise_likelihood] == 'VERY_LIKELY'):
-                                print('Person' + str(person) + ' is very likely to be surprised')
+                                message = ('Person' + str(person) + ' is very likely to be surprised')
                         elif (likelihood_name[face.surprise_likelihood] == 'LIKELY'):
-                                print('Person' + str(person) + ' is likely to be surprised')
+                                message = ('Person' + str(person) + ' is likely to be surprised')
                         else:
-                                print('Unable to determine person' + str(person))
+                                message = ('Unable to determine person' + str(person))
+                        draw.text((x, y), message, fill=color, font=font)
                         person += 1
-                if (x == 0):
-                        print('No face detected')
+                        x += 25
+                
+                if (flag == 0):
+                        message = ('No face detected')
+                        draw.text((x, y), message, fill=color, font=font)
+                addtxt = addtxt.save(os.path.join(path, str(file_name) + 'txt.jpg'))
                 if response.error.message:
                         raise Exception(
                                 '{}\nFor more info on error messages, check: '
                                 'https://cloud.google.com/apis/design/errors'.format(
                                         response.error.message))
                 file_name += 1
+
 def Image2Video(videoname, filepath1):
-        subprocess.call('ffmpeg -framerate 1/3 -start_number 1 -i '+ filepath1 +'%1d.jpg '+ videoname +'.avi')
+        subprocess.call('ffmpeg -framerate 1/3 -start_number 1 -i '+ filepath1 +'%1dtxt.jpg '+ videoname +'.avi')
+
